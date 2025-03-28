dc18ff7cac4f2fb58475ea7ade0f925ae9d17a6f
JimKarpodinis <dkarpodinis2@gmail.com>
2025-03-28 12:02:39 +0200

    feat: Create Validate Model Performance

---
+++ src/validate_model_performance.py
@@ -0,0 +1,38 @@
+"""
+Validate the performance of the models which was stated in the MentalBert paper.
+The models to be checked are BERT, RoBERTa, BioBERT, ClinicalBERT, MentalBERT, MentalRoBERTa
+"""
+
+from trasnformers import PretrainedModel
+import evaluate
+from datasets import Dataset
+
+
+def check_model_performance(model: PretrainedModel, dataset: Dataset) -> dict:
+    
+    labels = dataset.select_columns(["label"])
+    features = dataset.select_columns(["text"])
+    
+
+    logits = model(dataset)
+    
+    eval_preds = (logits, labels)
+    return compute_metrics(eval_preds)
+
+
+def compute_metrics(eval_preds):
+
+    accuracy_metric = evaluate.load("accuracy")
+    f1_metric = evaluate.load("f1")
+
+
+    logits, labels = eval_preds
+    preds = np.argmax(logits, axis=1)
+    
+
+    accuracy = accuracy_metric.compute(preds, labels)
+    f1 = f1_metric.compute(preds, labels)   
+
+
+    return {"accuracy": accuracy, "f1": f1}
+    
