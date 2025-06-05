57b4285738cd179ee937ce9e413263f516ae7f79
JimKarpodinis <dkarpodinis2@gmail.com>
2025-06-05 11:21:43 +0000

    feat: Implement hyperparamter tuning

--- src/dp_finetune_model.py
+++ src/dp_finetune_model.py
@@ -48,6 +48,30 @@ def compute_objective(trial):
 
     hp_space = optuna_hp_space(trial)
 
+    set_seed(seed)
+    training_args = define_training_args()
+
+    hf_token = os.getenv("HF_TOKEN")
+    privacy_engine = PrivacyEngine()#secure_mode=True)
+    #------------------------------------------------------------------------
+    dataset_dict = load_dataset("csv", data_dir=data_dir)
+
+    tokenizer = AutoTokenizer.from_pretrained(model_name, token=hf_token)
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
+
+    dataset_dict = dataset_dict.map(
+            lambda batch: tokenize_sentences(batch,
+                tokenizer), batched=True)
+
+    dataset_dict.set_format("torch", device=device)
+
+    training_args = define_training_args()
+
+    training_args.update(hp_space)
+
+    train(**training_args)
+
+
 
 
 def count_parameters(model):
@@ -139,32 +163,6 @@ def main(model_name: str, data_dir: str, seed: int, loss_fn=CrossEntropyLoss(red
     study = optuna.create_study()
     study.optimize(compute_objective, n_trials=200)
 
-    set_seed(seed)
-    training_args = define_training_args()
-
-    hf_token = os.getenv("HF_TOKEN")
-    privacy_engine = PrivacyEngine()#secure_mode=True)
-    #------------------------------------------------------------------------
-    dataset_dict = load_dataset("csv", data_dir=data_dir)
-
-    tokenizer = AutoTokenizer.from_pretrained(model_name, token=hf_token)
-    device = "cuda:0" if torch.cuda.is_available() else "cpu"
-
-    dataset_dict = dataset_dict.map(
-            lambda batch: tokenize_sentences(batch,
-                tokenizer), batched=True)
-
-    dataset_dict.set_format("torch", device=device)
-
-    training_args = define_training_args()
-
-    train(**training_args)
-
-    train_dataset = dataset_dict["train"]
-    num_labels = len(train_dataset["labels"].unique())
-
-    validation_dataset = dataset_dict["validation"]
-
 
 def train(model: BertForSequenceClassification,
         learning_rate: float,
@@ -175,24 +173,21 @@ def train(model: BertForSequenceClassification,
         **kwargs):
 
 
+    train_dataset = dataset_dict["train"]
+    num_labels = len(train_dataset["labels"].unique())
+    validation_dataset = dataset_dict["validation"]
+
     model = model_init(model_name, hf_token, num_labels)
     model_params = count_parameters(model)
 
-    optimizer = AdamW(model.classifier.parameters(), lr=learning_rate, eps=1e-8)
-
+    optimizer = get_optimizer(model, learning_rate)
     # scheduler = LinearLR(optimizer=optimizer) # , start_factor=0.1, gamma=0.01)
-
-    train_loader = DataLoader(train_dataset,
-            batch_size=training_args.per_device_train_batch_size,
-            shuffle=True) # , collate_fn=data_collator)
-
-    validation_loader = DataLoader(validation_dataset,
-            batch_size=training_args.per_device_eval_batch_size,
-            shuffle=True) # , collate_fn=data_collator)
+    train_loader = get_train_dataloader(train_dataset, train_batch_size)
+    validation_loader = get_validation_loader(validation_dataset, validation_batch_size)
 
     #----------------------------------------------------------------------------
 
-    timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+    # timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
     writer = SummaryWriter(training_args.logging_dir)
 
     EPOCHS = training_args.num_train_epochs
@@ -206,6 +201,8 @@ def train(model: BertForSequenceClassification,
     noise_multiplier=1.1,
     max_grad_norm=1.0,
     )
+
+    #----------------------------------------------------------------------------
 
     best_vloss = 1_000_000
 
@@ -223,10 +220,72 @@ def train(model: BertForSequenceClassification,
 
         writer.add_scalars('Random Seed', {"Seed": seed}, epoch + 1)
 
-        evaluate_(model, epoch, validation_loader)
+        best_model, epoch_best, best_vloss = evaluate_(model, epoch, validation_loader,
+                writer, privacy_engine, best_vloss)
+
+        if best_model is not None:
+
+            model_params["best_model"] = best_model
+            model_params["epoch_best"] = epoch_best
+            model_params["best_vloss"] = best_vloss
 
 
-def evaluate_(model: BertForSequenceClassification, epoch: int, validation_loader: DataLoader):
+        model_path = f"{training_args.output_dir}/seed={seed}"
+
+        write_training_metadata(model_path, model_params)
+
+
+def get_validation_dataloader(dataset: Dataset, batch_size: int) -> DataLoader:
+
+    return DataLoader(validation_dataset,
+            batch_size=training_args.per_device_eval_batch_size,
+            shuffle=True) # , collate_fn=data_collator)
+
+
+def get_train_dataloader(dataset: Dataset, batch_size: int) -> DataLoader:
+
+    return DataLoader(train_dataset,
+            batch_size=training_args.per_device_train_batch_size,
+            shuffle=True) # , collate_fn=data_collator)
+
+
+def get_optimizer(model: BertForSequenceClassification, lr: float)
+    return AdamW(model.classifier.parameters(), lr=lr, eps=1e-8)
+
+
+def write_training_metadata(model_path: str, model_params: dict):
+
+    os.makedirs(model_path, exist_ok=True)
+
+    with open(
+            os.path.join(
+                model_path, "training_metadata.json"), "w") as f:
+
+                json.dump(
+                        {"opacus_epsilon": model_params["best_epsilon"],
+                    "epoch": model_params["epoch_best"],
+                    "trainable_params": model_params["trainable_params"], 
+                    "total_params": model_params["total_params"],
+                    "non_trainable_params": model_params["non_trainable_params"]
+                    }, f)
+
+
+    # best_model = convert_grad_sample_modules(best_model)
+
+    best_model._module.save_pretrained(model_path, from_pt=True)
+    model.train(True)
+    print(f"(ε = {best_epsilon:.4f}, δ = {10**-5})")
+
+
+def evaluate_(
+        model: BertForSequenceClassification,
+        epoch: int,
+        validation_loader: DataLoader,
+        writer: SummaryWriter,
+        privacy_engine: PrivacyEngine,
+        best_vloss: float) -> tuple[
+        BertForSequenceClassification, int,
+        float]:
 
     running_vloss = 0.0
     # Set the model to evaluation mode, disabling dropout and using population
@@ -237,12 +296,14 @@ def evaluate_(model: BertForSequenceClassification, epoch: int, validation_loade
     with torch.no_grad():
         for i, vinputs in enumerate(validation_loader):
 
-            del vinputs["text"]
+            vinputs_ = vinputs
 
-            voutputs = model(**vinputs)
+            del vinputs_["text"]
+
+            voutputs = model(**vinputs_)
             vlogits = voutputs.logits
 
-            vlabels = vinputs["labels"]
+            vlabels = vinputs_["labels"]
 
             vloss = voutputs.loss
 
@@ -254,7 +315,7 @@ def evaluate_(model: BertForSequenceClassification, epoch: int, validation_loade
     # Log the running loss averaged per batch
     # for both training and validation
     writer.add_scalars('Training vs. Validation Loss',
-                    { 'Training' : avg_loss, 'Validation' : avg_vloss },
+                    {'Training' : avg_loss, 'Validation' : avg_vloss },
                     epoch + 1)
     writer.flush()
 
@@ -266,26 +327,15 @@ def evaluate_(model: BertForSequenceClassification, epoch: int, validation_loade
 
        best_model = model
        epoch_best = epoch + 1
+       best_vloss = avg_vloss
 
+    else:
+        best_model = None
+        epoch_best = None
+        best_vloss = None
+        
 
-model_path = f"{training_args.output_dir}/seed={seed}"
-os.makedirs(model_path, exist_ok=True)
-
-with open(
-        os.path.join(
-            model_path, "training_metadata.json"), "w") as f:
-
-            json.dump({"opacus_epsilon": best_epsilon, "epoch": epoch_best,
-                "trainable_params": model_params["trainable_params"], 
-                "total_params": model_params["total_params"],
-                "non_trainable_params": model_params["non_trainable_params"]}, f)
-
-
-# best_model = convert_grad_sample_modules(best_model)
-
-best_model._module.save_pretrained(model_path, from_pt=True)
-model.train(True)
-print(f"(ε = {best_epsilon:.4f}, δ = {10**-5})")
+   return best_model, epoch_best, best_vloss
 
 
 if __name__ == "__main__":
